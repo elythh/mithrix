ce46698d212e2969a8b0b0b922f06dd719dcd1b6
elythh <gwen@omg.lol>
2025-11-02 19:02:34 +0100

    minecraft i love minecraft

--- modules/home-manager/stacks/minecraft/default.nix
+++ modules/home-manager/stacks/minecraft/default.nix
@@ -16,22 +16,22 @@ in {
       };
     };
     services.podman.containers = {
-      # mc-proxy = {
-      #  image = "docker.io/itzg/mc-proxy";
-      #   ports= [ "25565:25577" "25577:25577/udp" ];
-      #   environment = {
-      #     TYPE = "VELOCITY";
-      #     UID = config.meadow.stacks.defaultUid;
-      #     GID = config.meadow.stacks.defaultGid;
-      #     TZ = config.meadow.stacks.defaultTz;
-      #     EXTRA_ARGS = "-Dvelocity.max-known-packs=101";
-      #   };
-      #   volumes = [
-      #     "${storage}/proxy:/server"
-      #   ];
-      #   network = [ "mc-backend"];
-      #
-      # };
+      mc-proxy = {
+       image = "docker.io/itzg/mc-proxy";
+        ports= [ "25565:25577" "25577:25577/udp" ];
+        environment = {
+          TYPE = "VELOCITY";
+          UID = config.meadow.stacks.defaultUid;
+          GID = config.meadow.stacks.defaultGid;
+          TZ = config.meadow.stacks.defaultTz;
+          EXTRA_ARGS = "-Dvelocity.max-known-packs=101";
+        };
+        volumes = [
+          "${storage}/proxy:/server"
+        ];
+        network = [ "mc-backend"];
+
+      };
       # bookyytown = {
       #  image = "docker.io/itzg/minecraft-server:latest";
       #   # ports= [ "25565:25565"];
@@ -53,27 +53,26 @@ in {
       #   ];
       #   network = [ "mc-backend"];
       # };
-      # adventure = {
-      #  image = "docker.io/itzg/minecraft-server:latest";
-      #   environment = {
-      #     VERSION =  "1.20.1";
-      #     EULA = "true";
-      #     CF_API_KEY = "$2a$10$BZZVwIQpNzAUAQOavsaLj.P8B8O3O/RW3uQ2WjkbINitf4UwmEb.q";
-      #     ONLINE_MODE= "TRUE";
-      #     INIT_MEMORY = "6G";
-      #     MAX_MEMORY = "24G";
-      #     TYPE = "FABRIC";
-      #     UID = config.meadow.stacks.defaultUid;
-      #     GID = config.meadow.stacks.defaultGid;
-      #     TZ = config.meadow.stacks.defaultTz;
-      #     JVM_XX_OPTS = "-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15";
-      #   };
-      #   ports= [ "25565:25565" ];
-      #   volumes = [
-      #     "${storage}/adventureplus:/data"
-      #   ];
-      #   network = [ "mc-backend"];
-      # };
+      adventure = {
+       image = "docker.io/itzg/minecraft-server:latest";
+        environment = {
+          VERSION =  "1.20.1";
+          EULA = "true";
+          ONLINE_MODE= "TRUE";
+          INIT_MEMORY = "6G";
+          MAX_MEMORY = "24G";
+          TYPE = "FORGE";
+          UID = config.meadow.stacks.defaultUid;
+          GID = config.meadow.stacks.defaultGid;
+          TZ = config.meadow.stacks.defaultTz;
+          JVM_XX_OPTS = "-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15";
+        };
+        # ports= [ "25565:25565" ];
+        volumes = [
+          "${storage}/ainoa:/data"
+        ];
+        network = [ "mc-backend"];
+      };
       # cinema = {
       #  image = "docker.io/itzg/minecraft-server:latest";
       #   # ports= [ "25565:25565"];
