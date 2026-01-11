5f03ba965a99c5d12abb1d65e8c534bb1fb7cae6
elythh <gwen@omg.lol>
2026-01-11 13:52:46 +0100

    feat: flake update
    also fix pocketid

--- modules/home-manager/stacks/minecraft/default.nix
+++ modules/home-manager/stacks/minecraft/default.nix
@@ -73,6 +73,26 @@ in {
         ];
         network = [ "mc-backend"];
       };
+      # escaperoom = {
+      #  image = "docker.io/itzg/minecraft-server:latest";
+      #   environment = {
+      #     VERSION =  "1.20.1";
+      #     EULA = "true";
+      #     ONLINE_MODE= "false";
+      #     INIT_MEMORY = "6G";
+      #     MAX_MEMORY = "24G";
+      #     TYPE = "PAPER";
+      #     UID = config.meadow.stacks.defaultUid;
+      #     GID = config.meadow.stacks.defaultGid;
+      #     TZ = config.meadow.stacks.defaultTz;
+      #     JVM_XX_OPTS = "-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15";
+      #   };
+      #   # ports= [ "25565:25565" ];
+      #   volumes = [
+      #     "${storage}/escaperoom:/data"
+      #   ];
+      #   network = [ "mc-backend"];
+      # };
       # cinema = {
       #  image = "docker.io/itzg/minecraft-server:latest";
       #   # ports= [ "25565:25565"];
