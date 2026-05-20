f954e83c80e1ba36be6cff57da23ed2dddbf6bfc
elythh <gwen@omg.lol>
2026-05-20 15:44:41 +0200

    fix: koito new version was crashing

--- modules/home-manager/stacks/koito/default.nix
+++ modules/home-manager/stacks/koito/default.nix
@@ -36,10 +36,10 @@ in {
           "${storage}/koito/data:/etc/koito"
         ];
           environment = {
-            KOITO_DATABASE_URL="postgres://postgres:secret_password@pg:5432/koitodb";
+            # KOITO_DATABASE_URL="postgres://postgres:secret_password@pg:5432/koitodb";
             KOITO_ALLOWED_HOSTS="koito.elyth.xyz,192.168.0.100:4110";
-            KOITO_SUBSONIC_URL="https://navidrome.elyth.xyz";
-            KOITO_SUBSONIC_PARAMS="u=elyth&t=4763f6aaeb37fbf45a208337220ec16b&s=081120";
+            # KOITO_SUBSONIC_URL="https://navidrome.elyth.xyz";
+            # KOITO_SUBSONIC_PARAMS="u=elyth&t=4763f6aaeb37fbf45a208337220ec16b&s=081120";
             KOITO_ENABLE_LBZ_RELAY="true";
             KOITO_LBZ_RELAY_URL="https://api.listenbrainz.org/1";
             KOITO_LBZ_RELAY_TOKEN="7c52576c-74f4-4726-96b1-a9a169a2f35e";
